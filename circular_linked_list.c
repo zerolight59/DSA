d6a5c068910f043e4ce98fb9c41cace43226f1bc
MANU SANKAR U <115739592+zerolight59@users.noreply.github.com>
2023-10-09 22:00:58 +0530

    added delete to circular linked list

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -30,6 +30,7 @@ void insert(int e){
 }
 
 void disp(){
+    printf("\n");
     cll *t=head;
     if(head==NULL){
         printf("list is empty");
@@ -45,12 +46,56 @@ void disp(){
     }
 }
 
+void delete(int e){
+    cll *t;
+
+    if (head == NULL)
+    {
+        printf("list is empty");
+    }
+
+    else if (head ->data == e && head->next==head)
+    {
+        head=NULL;
+    }
+
+    else if (head->data ==e)
+    {
+        t=head;
+        while (t->next !=head)
+        {
+            t=t->next;
+        }
+        t->next=head->next;
+        head=head->next;
+    }
+    else{
+        t=head;
+        while (t->next !=head && t->next->data==e)
+        {
+            t=t->next;
+        }
+        if (t->next==head)
+        {
+            printf("\nelement not found");
+        }
+        else{
+            t->next=t->next->next;
+        }
+    }
+    
+    
+}
+
 int main(){
     insert(10);
     insert(20);
     insert(30);
     insert(40);
     
+    disp();
+
+    delete(10);
     disp();
     
     return 0;
