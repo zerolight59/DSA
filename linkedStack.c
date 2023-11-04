8251b6e16d49c555b2fcce37ef8412a996aa0ff1
MANU SANKAR U <115739592+zerolight59@users.noreply.github.com>
2023-11-05 03:40:39 +0530

    Update linkedStack.c
    make menu driven

--- linkedStack.c
+++ linkedStack.c
@@ -20,11 +20,10 @@ void pop(){
         printf("empty list");
     }
     else{
-        printf("\nelment deleted is : %d",top->data);
+        printf("\nelment poped is : %d",top->data);
         top=top->next;
     }
 }
-
 
 void peek(){
     if(top==NULL){
@@ -34,15 +33,33 @@ void peek(){
         printf("\ntop element is : %d",top->data);
     }
 }
-int main(){
-    push(10);
-    push(20);
-    push(30);
-    
-    peek();
-    pop();
-    peek();
-    pop();
-    peek();
+int menu()
+{	
+	int ch;
+	printf("\n1 - push\t2 - pop\t3 - peek\t4 - exit");
+	printf("\nenter your choise :");
+	scanf("%d",&ch);
+	return ch;
+}
+void process()
+{
+	int ch,e;
+	for(ch =menu();ch!=4;ch=menu())
+	{
+		switch(ch)
+		{
+			case 1:
+                    printf("enter the value :");
+                    scanf("%d",&e);
+                    push(e);
+					break;
+            case 2:pop();break;
+			case 3:peek();break;
+		}
+	}
+}
+
+int main() {
+    process();
     return 0;
 }
